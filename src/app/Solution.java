fdddfb59ef0a0862147ce154276df89212b3507d
dmyt2024 <dmitrijnen@gmail.com>
2024-10-11 16:54:43 +0300

    Solution_02

---
+++ src/app/Solution.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Random;
+
+public class Solution {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int num = random.nextInt(100_000) + 11;
+        System.out.println("Рандомне число: " + num);
+        int num_len = String.valueOf(num).length();
+        int sum = 0;
+
+        for (int i = 0; i < num_len; i++) {
+            int nums = num % 10;
+            num /= 10;
+            sum += nums;
+            System.out.println(num);
+
+
+        }
+
+        System.out.println("Сума числа: " + sum);
+    }
+}
